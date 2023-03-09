6e80b98efe378513510df65844498bc642fb28e8
angusPhilippi <42793739+angusPhilippi@users.noreply.github.com>
2023-03-09 22:01:28 +1000

    Added colour change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import iro from '@jaames/iro';
 
 function App() {
   const canvasWidth = 680;
@@ -12,8 +13,12 @@ function App() {
   let endX: number = 0; // x-coordinate of ending point
   let endY: number = 0; // y-coordinate of ending point
 
+  const clearButton: HTMLButtonElement = document.getElementById('clearButton') as HTMLButtonElement;
   const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
-  const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!; // get the 2D rendering context of the canvas
+  const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
+  const colourPicker = iro.ColorPicker('#colourPicker', {
+    wheelLightness: false, width: 160, color: '#f00',
+  });
   const mousePositionContainer = document.getElementById('mousePosition');
 
   canvas.width = canvasWidth;
@@ -31,7 +36,7 @@ function App() {
     mouseY = event.clientY - rect.top;
 
     if (mousePositionContainer !== null) {
-      mousePositionContainer.innerHTML = `Mouse Position - X: ${mouseX}, Y: ${mouseY}`;
+      mousePositionContainer.innerHTML = `X: ${mouseX}, Y: ${mouseY}`;
     }
   });
 
@@ -44,11 +49,15 @@ function App() {
       endX = event.offsetX; // get ending x-coordinate relative to canvas
       endY = event.offsetY; // get ending y-coordinate relative to canvas
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
       ctx.beginPath();
+      ctx.strokeStyle = colourPicker.color.hexString;
       ctx.rect(startX, startY, endX - startX, endY - startY); // create rectangle
       ctx.stroke(); // draw rectangle
     }
+  });
+
+  clearButton.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
   });
 
   canvas.addEventListener('click', (event: MouseEvent) => {
