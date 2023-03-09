d891de3bb143a0b10f353a31379f0bb50d31a9c9
angusPhilippi <42793739+angusPhilippi@users.noreply.github.com>
2023-03-09 20:51:42 +1000

    added basic canvas and mouse controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const canvasWidth = 680;
+  const canvasHeight = 480;
+
+  let mouseX: number = 0;
+  let mouseY: number = 0;
+  let isDrawing: boolean = false; // flag to indicate if we are currently drawing
+  let startX: number = 0; // x-coordinate of starting point
+  let startY: number = 0; // y-coordinate of starting point
+  let endX: number = 0; // x-coordinate of ending point
+  let endY: number = 0; // y-coordinate of ending point
+
+  const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+  const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!; // get the 2D rendering context of the canvas
+  const mousePositionContainer = document.getElementById('mousePosition');
+
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+
+  canvas.addEventListener('mousedown', (event: MouseEvent) => {
+    isDrawing = true; // set flag to indicate we are starting to draw
+    startX = event.offsetX; // get starting x-coordinate relative to canvas
+    startY = event.offsetY; // get starting y-coordinate relative to canvas
+  });
+
+  canvas.addEventListener('mousemove', (event: MouseEvent) => {
+    const rect: DOMRect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
+
+    if (mousePositionContainer !== null) {
+      mousePositionContainer.innerHTML = `Mouse Position - X: ${mouseX}, Y: ${mouseY}`;
+    }
+  });
+
+  canvas.addEventListener('mouseup', () => {
+    isDrawing = false; // set flag to indicate we have finished drawing
+  });
+
+  canvas.addEventListener('mousemove', (event: MouseEvent) => {
+    if (isDrawing) {
+      endX = event.offsetX; // get ending x-coordinate relative to canvas
+      endY = event.offsetY; // get ending y-coordinate relative to canvas
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+      ctx.beginPath();
+      ctx.rect(startX, startY, endX - startX, endY - startY); // create rectangle
+      ctx.stroke(); // draw rectangle
+    }
+  });
+
+  canvas.addEventListener('click', (event: MouseEvent) => {
+    const rect: DOMRect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.tsx
-          </code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
