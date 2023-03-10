72473661b283caae12356a429155f9c85c0a0f3d
angusPhilippi <42793739+angusPhilippi@users.noreply.github.com>
2023-03-10 23:05:43 +1000

    Added colour change

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,23 @@ function App() {
   let endX: number = 0; // x-coordinate of ending point
   let endY: number = 0; // y-coordinate of ending point
 
+  let inputStartX: number = 0;
+  let inputStartY: number = 0;
+
+  let inputEndX: number = 0;
+  let inputEndY: number = 0;
+
+  const inputStartXElement: HTMLInputElement = document.getElementById('inputStartX') as HTMLInputElement;
+  const inputStartYElement: HTMLInputElement = document.getElementById('inputStartY') as HTMLInputElement;
+  const inputEndXElement: HTMLInputElement = document.getElementById('inputEndX') as HTMLInputElement;
+  const inputEndYElement: HTMLInputElement = document.getElementById('inputEndY') as HTMLInputElement;
+
   const clearButton: HTMLButtonElement = document.getElementById('clearButton') as HTMLButtonElement;
+  const rectangleButton: HTMLButtonElement = document.getElementById('drawRectangleButton') as HTMLButtonElement;
+  const lineButton: HTMLButtonElement = document.getElementById('drawLineButton') as HTMLButtonElement;
+  const circleButton: HTMLButtonElement = document.getElementById('drawCircleButton') as HTMLButtonElement;
+  const pointButton: HTMLButtonElement = document.getElementById('drawPointButton') as HTMLButtonElement;
+
   const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
   const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
   const colourPicker = iro.ColorPicker('#colourPicker', {
@@ -58,6 +74,49 @@ function App() {
 
   clearButton.addEventListener('click', () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+  });
+
+  rectangleButton.addEventListener('click', () => {
+    ctx.strokeStyle = colourPicker.color.hexString;
+    ctx.rect(inputStartX, inputStartY, inputEndX - inputStartX, inputEndY - inputStartY);
+    console.log(`X:${inputStartX} Y:${inputStartY}, X: ${inputEndX}Y: ${inputEndY}`);
+  });
+
+  lineButton.addEventListener('click', () => {
+  });
+
+  circleButton.addEventListener('click', () => {
+  });
+
+  pointButton.addEventListener('click', () => {
+  });
+
+  inputStartXElement.addEventListener('change', () => {
+    const value = document.querySelector('input')?.value;
+    if (value !== undefined) {
+      inputStartX = parseFloat(value);
+    }
+  });
+
+  inputStartYElement.addEventListener('change', () => {
+    const value = document.querySelector('input')?.value;
+    if (value !== undefined) {
+      inputStartY = parseFloat(value);
+    }
+  });
+
+  inputEndXElement.addEventListener('change', () => {
+    const value = document.querySelector('input')?.value;
+    if (value !== undefined) {
+      inputEndX = parseFloat(value);
+    }
+  });
+
+  inputEndYElement.addEventListener('change', () => {
+    const value = document.querySelector('input')?.value;
+    if (value !== undefined) {
+      inputEndY = parseFloat(value);
+    }
   });
 
   canvas.addEventListener('click', (event: MouseEvent) => {
