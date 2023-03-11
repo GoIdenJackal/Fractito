11ec5918547577cd328a731d273ccbc23d834752
angusPhilippi <42793739+angusPhilippi@users.noreply.github.com>
2023-03-11 22:56:30 +1000

    Cleaned up and stylised

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,22 @@ import './App.css';
 import iro from '@jaames/iro';
 
 function App() {
-  const canvasWidth = 680;
-  const canvasHeight = 480;
+  const canvasWidth = 800;
+  const canvasHeight = 800;
+
+  let currentToolSelected: string;
 
   let mouseX: number = 0;
   let mouseY: number = 0;
-  let isDrawing: boolean = false; // flag to indicate if we are currently drawing
   let startX: number = 0; // x-coordinate of starting point
   let startY: number = 0; // y-coordinate of starting point
   let endX: number = 0; // x-coordinate of ending point
   let endY: number = 0; // y-coordinate of ending point
 
-  let inputStartX: number = 0;
-  let inputStartY: number = 0;
+  let currentLineWidth: number = 0;
 
-  let inputEndX: number = 0;
-  let inputEndY: number = 0;
-
-  const inputStartXElement: HTMLInputElement = document.getElementById('inputStartX') as HTMLInputElement;
-  const inputStartYElement: HTMLInputElement = document.getElementById('inputStartY') as HTMLInputElement;
-  const inputEndXElement: HTMLInputElement = document.getElementById('inputEndX') as HTMLInputElement;
-  const inputEndYElement: HTMLInputElement = document.getElementById('inputEndY') as HTMLInputElement;
+  const inputLineWidthElement: HTMLInputElement = document.getElementById('lineWidth') as HTMLInputElement;
+  inputLineWidthElement.value = '0';
 
   const clearButton: HTMLButtonElement = document.getElementById('clearButton') as HTMLButtonElement;
   const rectangleButton: HTMLButtonElement = document.getElementById('drawRectangleButton') as HTMLButtonElement;
@@ -30,18 +25,27 @@ function App() {
   const circleButton: HTMLButtonElement = document.getElementById('drawCircleButton') as HTMLButtonElement;
   const pointButton: HTMLButtonElement = document.getElementById('drawPointButton') as HTMLButtonElement;
 
+  const currentToolSelectedElement = document.getElementById('currentToolSelected');
+
   const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+
   const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
+
   const colourPicker = iro.ColorPicker('#colourPicker', {
     wheelLightness: false, width: 160, color: '#f00',
   });
-  const mousePositionContainer = document.getElementById('mousePosition');
+
+  const mousePositionText = document.getElementById('mousePosition');
+  const canvasDimensionsText = document.getElementById('canvasDimensions');
 
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
 
+  if (canvasDimensionsText !== null) {
+    canvasDimensionsText.innerHTML = `${canvasWidth} x ${canvasHeight}`;
+  }
+
   canvas.addEventListener('mousedown', (event: MouseEvent) => {
-    isDrawing = true; // set flag to indicate we are starting to draw
     startX = event.offsetX; // get starting x-coordinate relative to canvas
     startY = event.offsetY; // get starting y-coordinate relative to canvas
   });
@@ -51,24 +55,62 @@ function App() {
     mouseX = event.clientX - rect.left;
     mouseY = event.clientY - rect.top;
 
-    if (mousePositionContainer !== null) {
-      mousePositionContainer.innerHTML = `X: ${mouseX}, Y: ${mouseY}`;
+    if (mouseX > canvasWidth) {
+      mouseX = 400;
+    } else if (mouseX < 0) {
+      mouseX = 0;
+    }
+
+    if (mouseY > canvasHeight) {
+      mouseY = 400;
+    } else if (mouseX < 0) {
+      mouseY = 0;
+    }
+
+    if (mousePositionText !== null) {
+      mousePositionText.style.visibility = 'visible';
+      mousePositionText.innerHTML = `X: ${Math.round(mouseX)}, Y: ${Math.round(mouseY)}`;
     }
   });
 
+  function distance(x1: number, y1: number, x2: number, y2: number) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
   canvas.addEventListener('mouseup', () => {
-    isDrawing = false; // set flag to indicate we have finished drawing
-  });
+    endX = mouseX;
+    endY = mouseY;
+    ctx.strokeStyle = colourPicker.color.hexString;
+    ctx.fillStyle = colourPicker.color.hexString;
 
-  canvas.addEventListener('mousemove', (event: MouseEvent) => {
-    if (isDrawing) {
-      endX = event.offsetX; // get ending x-coordinate relative to canvas
-      endY = event.offsetY; // get ending y-coordinate relative to canvas
+    switch (currentToolSelected) {
+      case 'Rectangle':
+        ctx.beginPath();
+        ctx.rect(startX, startY, endX - startX, endY - startY);
+        ctx.stroke();
+        break;
+      case 'Circle':
+        ctx.beginPath();
+        ctx.arc(startX, startY, distance(startX, startY, endX, endY), 0, 2 * Math.PI, false);
+        ctx.stroke();
+        break;
+      case 'Line':
+        ctx.lineWidth = currentLineWidth;
+        ctx.beginPath();
+        ctx.moveTo(startX, startY);
+        ctx.lineTo(endX, endY);
+        ctx.stroke();
+        break;
+      case 'Point':
+        ctx.beginPath();
+        ctx.fillRect(endX - 1, endY - 1, 2, 2);
+        ctx.stroke();
+        break;
 
-      ctx.beginPath();
-      ctx.strokeStyle = colourPicker.color.hexString;
-      ctx.rect(startX, startY, endX - startX, endY - startY); // create rectangle
-      ctx.stroke(); // draw rectangle
+      default:
+        break;
     }
   });
 
@@ -77,52 +119,43 @@ function App() {
   });
 
   rectangleButton.addEventListener('click', () => {
-    ctx.strokeStyle = colourPicker.color.hexString;
-    ctx.rect(inputStartX, inputStartY, inputEndX - inputStartX, inputEndY - inputStartY);
-    console.log(`X:${inputStartX} Y:${inputStartY}, X: ${inputEndX}Y: ${inputEndY}`);
+    currentToolSelected = 'Rectangle';
+    if (currentToolSelectedElement !== null) {
+      currentToolSelectedElement.style.visibility = 'visible';
+      currentToolSelectedElement.innerHTML = `${currentToolSelected} Tool`;
+    }
   });
 
   lineButton.addEventListener('click', () => {
+    currentToolSelected = 'Line';
+    if (currentToolSelectedElement !== null) {
+      currentToolSelectedElement.style.visibility = 'visible';
+      currentToolSelectedElement.innerHTML = `${currentToolSelected} Tool`;
+    }
   });
 
   circleButton.addEventListener('click', () => {
+    currentToolSelected = 'Circle';
+    if (currentToolSelectedElement !== null) {
+      currentToolSelectedElement.style.visibility = 'visible';
+      currentToolSelectedElement.innerHTML = `${currentToolSelected} Tool`;
+    }
   });
 
   pointButton.addEventListener('click', () => {
-  });
-
-  inputStartXElement.addEventListener('change', () => {
-    const value = document.querySelector('input')?.value;
-    if (value !== undefined) {
-      inputStartX = parseFloat(value);
+    currentToolSelected = 'Point';
+    if (currentToolSelectedElement !== null) {
+      currentToolSelectedElement.style.visibility = 'visible';
+      currentToolSelectedElement.innerHTML = `${currentToolSelected} Tool`;
     }
   });
 
-  inputStartYElement.addEventListener('change', () => {
-    const value = document.querySelector('input')?.value;
+  inputLineWidthElement.addEventListener('input', () => {
+    const { value } = inputLineWidthElement;
     if (value !== undefined) {
-      inputStartY = parseFloat(value);
+      currentLineWidth = parseFloat(value);
+      ctx.lineWidth = currentLineWidth;
     }
-  });
-
-  inputEndXElement.addEventListener('change', () => {
-    const value = document.querySelector('input')?.value;
-    if (value !== undefined) {
-      inputEndX = parseFloat(value);
-    }
-  });
-
-  inputEndYElement.addEventListener('change', () => {
-    const value = document.querySelector('input')?.value;
-    if (value !== undefined) {
-      inputEndY = parseFloat(value);
-    }
-  });
-
-  canvas.addEventListener('click', (event: MouseEvent) => {
-    const rect: DOMRect = canvas.getBoundingClientRect();
-    mouseX = event.clientX - rect.left;
-    mouseY = event.clientY - rect.top;
   });
 
   return (
